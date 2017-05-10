f387afa3e5f50b5fdf986d4f307034c4ec4cada0
swaphack <809406730@qq.com>
2017-05-10 18:14:09 +0800

    add db

---
+++ mysql/shop_mommodity.sql
@@ -0,0 +1,315 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : webchat
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2017-05-10 18:13:23
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for auth_group
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_group`;
+CREATE TABLE `auth_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_group
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for auth_group_permissions
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_group_permissions`;
+CREATE TABLE `auth_group_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_group_permissions
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for auth_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_permission`;
+CREATE TABLE `auth_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
+  CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_permission
+-- ----------------------------
+INSERT INTO `auth_permission` VALUES ('1', 'Can add log entry', '1', 'add_logentry');
+INSERT INTO `auth_permission` VALUES ('2', 'Can change log entry', '1', 'change_logentry');
+INSERT INTO `auth_permission` VALUES ('3', 'Can delete log entry', '1', 'delete_logentry');
+INSERT INTO `auth_permission` VALUES ('4', 'Can add user', '2', 'add_user');
+INSERT INTO `auth_permission` VALUES ('5', 'Can change user', '2', 'change_user');
+INSERT INTO `auth_permission` VALUES ('6', 'Can delete user', '2', 'delete_user');
+INSERT INTO `auth_permission` VALUES ('7', 'Can add permission', '3', 'add_permission');
+INSERT INTO `auth_permission` VALUES ('8', 'Can change permission', '3', 'change_permission');
+INSERT INTO `auth_permission` VALUES ('9', 'Can delete permission', '3', 'delete_permission');
+INSERT INTO `auth_permission` VALUES ('10', 'Can add group', '4', 'add_group');
+INSERT INTO `auth_permission` VALUES ('11', 'Can change group', '4', 'change_group');
+INSERT INTO `auth_permission` VALUES ('12', 'Can delete group', '4', 'delete_group');
+INSERT INTO `auth_permission` VALUES ('13', 'Can add content type', '5', 'add_contenttype');
+INSERT INTO `auth_permission` VALUES ('14', 'Can change content type', '5', 'change_contenttype');
+INSERT INTO `auth_permission` VALUES ('15', 'Can delete content type', '5', 'delete_contenttype');
+INSERT INTO `auth_permission` VALUES ('16', 'Can add session', '6', 'add_session');
+INSERT INTO `auth_permission` VALUES ('17', 'Can change session', '6', 'change_session');
+INSERT INTO `auth_permission` VALUES ('18', 'Can delete session', '6', 'delete_session');
+INSERT INTO `auth_permission` VALUES ('19', 'Can add user', '7', 'add_user');
+INSERT INTO `auth_permission` VALUES ('20', 'Can change user', '7', 'change_user');
+INSERT INTO `auth_permission` VALUES ('21', 'Can delete user', '7', 'delete_user');
+INSERT INTO `auth_permission` VALUES ('22', 'Can add order', '8', 'add_order');
+INSERT INTO `auth_permission` VALUES ('23', 'Can change order', '8', 'change_order');
+INSERT INTO `auth_permission` VALUES ('24', 'Can delete order', '8', 'delete_order');
+INSERT INTO `auth_permission` VALUES ('25', 'Can add mommodity', '9', 'add_mommodity');
+INSERT INTO `auth_permission` VALUES ('26', 'Can change mommodity', '9', 'change_mommodity');
+INSERT INTO `auth_permission` VALUES ('27', 'Can delete mommodity', '9', 'delete_mommodity');
+
+-- ----------------------------
+-- Table structure for auth_user
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_user`;
+CREATE TABLE `auth_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_user
+-- ----------------------------
+INSERT INTO `auth_user` VALUES ('1', 'pbkdf2_sha256$36000$YYyzvuEtjcRf$9/UkxPtK5JoWvNRUGGj6OFxrRXco8/UdF3jaC0GUpGU=', '2017-05-10 05:35:18.253167', '1', 'root', '', '', '809406730@qq.com', '1', '1', '2017-05-10 05:34:35.473248');
+
+-- ----------------------------
+-- Table structure for auth_user_groups
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_user_groups`;
+CREATE TABLE `auth_user_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`),
+  CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_user_groups
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for auth_user_user_permissions
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_user_user_permissions`;
+CREATE TABLE `auth_user_user_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of auth_user_user_permissions
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for django_admin_log
+-- ----------------------------
+DROP TABLE IF EXISTS `django_admin_log`;
+CREATE TABLE `django_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL,
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
+  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of django_admin_log
+-- ----------------------------
+INSERT INTO `django_admin_log` VALUES ('1', '2017-05-10 05:36:06.715861', '1', 'Mommodity object', '1', '[{\"added\": {}}]', '9', '1');
+INSERT INTO `django_admin_log` VALUES ('2', '2017-05-10 05:36:45.916389', '2', 'Mommodity object', '1', '[{\"added\": {}}]', '9', '1');
+INSERT INTO `django_admin_log` VALUES ('3', '2017-05-10 06:56:01.781622', '2', 'Mommodity object', '2', '[{\"changed\": {\"fields\": [\"piture\"]}}]', '9', '1');
+INSERT INTO `django_admin_log` VALUES ('4', '2017-05-10 06:56:06.466867', '1', 'Mommodity object', '2', '[{\"changed\": {\"fields\": [\"piture\"]}}]', '9', '1');
+
+-- ----------------------------
+-- Table structure for django_content_type
+-- ----------------------------
+DROP TABLE IF EXISTS `django_content_type`;
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of django_content_type
+-- ----------------------------
+INSERT INTO `django_content_type` VALUES ('1', 'admin', 'logentry');
+INSERT INTO `django_content_type` VALUES ('4', 'auth', 'group');
+INSERT INTO `django_content_type` VALUES ('3', 'auth', 'permission');
+INSERT INTO `django_content_type` VALUES ('2', 'auth', 'user');
+INSERT INTO `django_content_type` VALUES ('5', 'contenttypes', 'contenttype');
+INSERT INTO `django_content_type` VALUES ('6', 'sessions', 'session');
+INSERT INTO `django_content_type` VALUES ('9', 'shop', 'mommodity');
+INSERT INTO `django_content_type` VALUES ('8', 'shop', 'order');
+INSERT INTO `django_content_type` VALUES ('7', 'webchat', 'user');
+
+-- ----------------------------
+-- Table structure for django_migrations
+-- ----------------------------
+DROP TABLE IF EXISTS `django_migrations`;
+CREATE TABLE `django_migrations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of django_migrations
+-- ----------------------------
+INSERT INTO `django_migrations` VALUES ('1', 'contenttypes', '0001_initial', '2017-05-10 05:33:42.676572');
+INSERT INTO `django_migrations` VALUES ('2', 'auth', '0001_initial', '2017-05-10 05:33:54.979714');
+INSERT INTO `django_migrations` VALUES ('3', 'admin', '0001_initial', '2017-05-10 05:33:57.840209');
+INSERT INTO `django_migrations` VALUES ('4', 'admin', '0002_logentry_remove_auto_add', '2017-05-10 05:33:57.963903');
+INSERT INTO `django_migrations` VALUES ('5', 'contenttypes', '0002_remove_content_type_name', '2017-05-10 05:34:00.025421');
+INSERT INTO `django_migrations` VALUES ('6', 'auth', '0002_alter_permission_name_max_length', '2017-05-10 05:34:00.922765');
+INSERT INTO `django_migrations` VALUES ('7', 'auth', '0003_alter_user_email_max_length', '2017-05-10 05:34:04.273268');
+INSERT INTO `django_migrations` VALUES ('8', 'auth', '0004_alter_user_username_opts', '2017-05-10 05:34:04.401347');
+INSERT INTO `django_migrations` VALUES ('9', 'auth', '0005_alter_user_last_login_null', '2017-05-10 05:34:05.143380');
+INSERT INTO `django_migrations` VALUES ('10', 'auth', '0006_require_contenttypes_0002', '2017-05-10 05:34:05.185382');
+INSERT INTO `django_migrations` VALUES ('11', 'auth', '0007_alter_validators_add_error_messages', '2017-05-10 05:34:05.232347');
+INSERT INTO `django_migrations` VALUES ('12', 'auth', '0008_alter_user_username_max_length', '2017-05-10 05:34:06.165016');
+INSERT INTO `django_migrations` VALUES ('13', 'sessions', '0001_initial', '2017-05-10 05:34:07.217194');
+INSERT INTO `django_migrations` VALUES ('14', 'shop', '0001_initial', '2017-05-10 05:34:08.143057');
+INSERT INTO `django_migrations` VALUES ('15', 'webchat', '0001_initial', '2017-05-10 05:34:08.793819');
+
+-- ----------------------------
+-- Table structure for django_session
+-- ----------------------------
+DROP TABLE IF EXISTS `django_session`;
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL,
+  PRIMARY KEY (`session_key`),
+  KEY `django_session_expire_date_a5c62663` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of django_session
+-- ----------------------------
+INSERT INTO `django_session` VALUES ('65tziucqdlk8b6gpcdoech6s7kxzu12r', 'YjIzODQ1MDJlODYyZDZlNTM1ZTE4ZjgwNmQ0MTg2ZTE5ZGY4OWU0Njp7Il9hdXRoX3VzZXJfYmFja2VuZCI6ImRqYW5nby5jb250cmliLmF1dGguYmFja2VuZHMuTW9kZWxCYWNrZW5kIiwiX2F1dGhfdXNlcl9oYXNoIjoiMjI4NWEzNTExYjhkN2UyZjQ5ZmZiNGU3NTBlODdhYzRlMmYyMTljMyIsIl9hdXRoX3VzZXJfaWQiOiIxIn0=', '2017-05-24 05:35:18.437385');
+
+-- ----------------------------
+-- Table structure for shop_mommodity
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_mommodity`;
+CREATE TABLE `shop_mommodity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `price` double NOT NULL,
+  `num` int(11) NOT NULL,
+  `detail` longtext NOT NULL,
+  `piture` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of shop_mommodity
+-- ----------------------------
+INSERT INTO `shop_mommodity` VALUES ('1', '水库', '100', '1', '存水', 'images/items/1.png');
+INSERT INTO `shop_mommodity` VALUES ('2', '矿场', '10', '1', '生产矿石', 'images/items/1_4.png');
+
+-- ----------------------------
+-- Table structure for shop_order
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_order`;
+CREATE TABLE `shop_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `custom_id` varchar(20) NOT NULL,
+  `custom_address` varchar(100) NOT NULL,
+  `mommodity_id` int(11) NOT NULL,
+  `mommodity_name` varchar(20) NOT NULL,
+  `num` int(11) NOT NULL,
+  `timestamp` datetime(6) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `msg` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of shop_order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for webchat_user
+-- ----------------------------
+DROP TABLE IF EXISTS `webchat_user`;
+CREATE TABLE `webchat_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of webchat_user
+-- ----------------------------
